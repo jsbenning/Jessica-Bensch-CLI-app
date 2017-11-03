bfbd05bf564a294375db96749bd95878fd519005
Jessica Bensch <github email address>
2017-11-02 22:55:13 -0400

    Add welcome prompts

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,18 @@
 # CLI Controller
 class GoldPrice::CLI
 
+  attr_accessor :metal
+
   def call
     puts "Welcome to Gold Price!"
+    puts "Please enter the number of the metal you would like to see the price of:"
+    puts "1. Gold"
+    puts "2. Silver"
+    @metal = gets.strip.to_i
+    puts "Please enter the number of the measurement you would like to use:"
+    puts "1. Grams"
+    puts "2. Ounces"
+    @measurement = gets.strip.to_i
   end
 
 end
