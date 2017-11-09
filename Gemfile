1f21193b564ea207de04ca619a007d1df9459794
Jessica Bensch <jessica.bensch11@gmail.com>
2017-11-09 10:08:17 -0500

    update file path

--- Gemfile
+++ Gemfile
@@ -4,4 +4,3 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in gold_price.gemspec
 gemspec
-gem 'gold_price'
--- lib/gold_price.rb
+++ lib/gold_price.rb
@@ -3,5 +3,5 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative "./gold_price/version"
-require_relative"./gold_price/cli.rb"
+require_relative "./gold_price/cli.rb"
 require_relative "./gold_price/price.rb"
